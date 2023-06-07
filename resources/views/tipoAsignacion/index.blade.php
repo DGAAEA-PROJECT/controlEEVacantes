01f816773cc54497fab68e9cf23f95498e81e6bd
PabloLUC9832 <lucaspablo340@outlook.com>
2023-06-06 18:17:18 -0600

    correccion error de paginacionó

--- resources/views/tipoAsignacion/index.blade.php
+++ resources/views/tipoAsignacion/index.blade.php
@@ -92,25 +92,25 @@
                     </td>
                 </tr>
             @else
-                @foreach($tiposAsignacion as $tiposAsignacion)
+                @foreach($tiposAsignacion as $tipoAsignacion)
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
 
                         <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {{$tiposAsignacion->tipo}}
+                            {{$tipoAsignacion->tipo}}
                         </th>
 
                         <td class="py-4 px-6">
-                            {{$tiposAsignacion->descripcion}}
+                            {{$tipoAsignacion->descripcion}}
                         </td>
 
                         <td class="py-4 px-6 text-right">
-                            <a href="{{route('tipoAsignacion.edit',$tiposAsignacion->id)}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar</a>
+                            <a href="{{route('tipoAsignacion.edit',$tipoAsignacion->id)}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar</a>
                         </td>
 
                         <td class="py-4 px-6 text-right">
                             <button type="button"
                                     class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                                    data-modal-toggle="delete-modal{{$tiposAsignacion->id}}">Eliminar</button>
+                                    data-modal-toggle="delete-modal{{$tipoAsignacion->id}}">Eliminar</button>
                         </td>
 
                     </tr>
